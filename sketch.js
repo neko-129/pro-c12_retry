3af20c960731a799b66d1b3e3871a39f412635a2
neko-129 <137789050+neko-129@users.noreply.github.com>
2023-11-27 17:21:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,11 +39,11 @@ function draw() {
   
  //var select_sprites = Math(random(1,3));
 
- var select_sprites = Math.random(random(1,3));
+ //var select_sprites = Math.random(random(1,3));
 
  //var select_sprites = Math.round(1,3);
 
- //var select_sprites = Math.round(random(1,3));
+ var select_sprites = Math.round(random(1,3));
 
   
   // if (frameCount % 50 == 0) {
